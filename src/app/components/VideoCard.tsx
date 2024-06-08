df8a53b2503c3b2b3487062757ba7edc6d1677e1
Peter Koverda <peter@pulsegrow.com>
2024-06-08 16:15:26 -0700

    link for vid card

--- src/app/components/VideoCard.tsx
+++ src/app/components/VideoCard.tsx
@@ -1,19 +1,24 @@
 import React from 'react';
 import { VideoCardProps } from "../common/types";
+import Link from "next/link";
 
 const VideoCard: React.FC<VideoCardProps> = ({ video }) => {
     return (
-        <div className="border p-4 mb-4">
-            <img
-                src={video.video_url}
-                alt={video.title}
-                className="w-full"
-            />
-            <h2 className="text-lg font-bold mt-2">{video.title}</h2>
-            <p>{video.user_id}</p>
-            <p>{new Date(video.created_at).toLocaleDateString()}</p>
-            <p>{video.num_comments}</p>
-        </div>
+        // todo: link by id
+        <Link href="videos/1">
+
+            <div className="border p-4 mb-4">
+                <img
+                    src={video.video_url}
+                    alt={video.title}
+                    className="w-full"
+                />
+                <h2 className="text-lg font-bold mt-2">{video.title}</h2>
+                <p>{video.user_id}</p>
+                <p>{new Date(video.created_at).toLocaleDateString()}</p>
+                <p>{video.num_comments}</p>
+            </div>
+        </Link>
     );
 };
 
