5ead215a9107ac98ba4416204a8b5803dd20e3e6
Peter Koverda <peter@pulsegrow.com>
2024-06-08 15:11:57 -0700

    update root and video[id] page

--- src/app/videos/[id]/page.tsx
+++ src/app/videos/[id]/page.tsx
@@ -1,4 +1,4 @@
-export default async function Page({ params }: { params: { id: string } }) {
+export default function Page({ params }: { params: { id: string } }) {
     const id = params.id;
     return <p>Video {id} Page</p>;
 }
