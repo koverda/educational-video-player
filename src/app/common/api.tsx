d9d6d5246c7523eb8f14a2c88e3a0738a7db325d
Peter Koverda <peter@pulsegrow.com>
2024-06-08 15:02:30 -0700

    first pass at api

---
+++ src/app/common/api.tsx
@@ -0,0 +1,69 @@
+import { Comment, Video} from "./types"
+
+const BASE_URL = 'https://take-home-assessment-423502.uc.r.appspot.com/api';
+
+export async function getUserVideos(userId: string): Promise<Video[]> {
+    const response = await fetch(`${BASE_URL}/videos?user_id=${userId}`);
+    if (!response.ok) {
+        throw new Error('Failed to fetch videos');
+    }
+    return await response.json();
+}
+
+export async function getSingleVideo(videoId: string): Promise<Video> {
+    const response = await fetch(`${BASE_URL}/videos/single?video_id=${videoId}`);
+    if (!response.ok) {
+        throw new Error('Failed to fetch video');
+    }
+    return await response.json();
+}
+
+export async function createVideo(video: Omit<Video, 'id' | 'created_at' | 'num_comments'>): Promise<Video> {
+    const response = await fetch(`${BASE_URL}/videos`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(video)
+    });
+    if (!response.ok) {
+        throw new Error('Failed to create video');
+    }
+    return await response.json();
+}
+
+export async function editVideo(video: Omit<Video, 'created_at' | 'num_comments'>): Promise<Video> {
+    const response = await fetch(`${BASE_URL}/videos`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(video)
+    });
+    if (!response.ok) {
+        throw new Error('Failed to edit video');
+    }
+    return await response.json();
+}
+
+export async function getVideoComments(videoId: string): Promise<Comment[]> {
+    const response = await fetch(`${BASE_URL}/videos/comments?video_id=${videoId}`);
+    if (!response.ok) {
+        throw new Error('Failed to fetch comments');
+    }
+    return await response.json();
+}
+
+export async function createComment(comment: Omit<Comment, 'id' | 'created_at'>): Promise<Comment> {
+    const response = await fetch(`${BASE_URL}/videos/comments`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(comment)
+    });
+    if (!response.ok) {
+        throw new Error('Failed to create comment');
+    }
+    return await response.json();
+}
