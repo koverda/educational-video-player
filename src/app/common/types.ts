cd33234d4e2be4941fd6509ace0d6584e09ff99a
Peter Koverda <peter@pulsegrow.com>
2024-06-08 14:59:59 -0700

    video and comment interfaces

---
+++ src/app/common/types.ts
@@ -0,0 +1,17 @@
+interface Video {
+    id: string;
+    created_at: string;
+    video_url: string;
+    user_id: string;
+    description: string;
+    title: string;
+    num_comments: number;
+}
+
+interface Comment {
+    id: string;
+    video_id: string;
+    user_id: string;
+    content: string;
+    created_at: string;
+}
