74919562319e6085093de9760d50bc5649f07d36
Peter Koverda <peter@pulsegrow.com>
2024-06-09 12:35:35 -0700

    video cards random with placeholders

--- src/app/components/VideoCard.tsx
+++ src/app/components/VideoCard.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import { VideoCardProps } from "../common/types";
 import Link from "next/link";
+import { getRandomInt } from "../common/util";
 
 const VideoCard: React.FC<VideoCardProps> = ({ video }) => {
+    const randId = getRandomInt(1, 50);
+
     return (
-        // todo: link by id
-        <Link href="videos/1">
+        <Link href={`/videos/${video.id}`} passHref>
 
             <div className="border p-4 mb-4">
                 <img
-                    src="https://picsum.photos/300/172"
+                    src={`https://picsum.photos/id/${randId}/300/172`}
                     alt={video.title}
                     className="w-full"
                 />
